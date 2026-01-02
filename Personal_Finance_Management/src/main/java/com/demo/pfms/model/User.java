4f6776f76a2e21f182d72251fde702f7cf776d81
lokeshbalaji906 <lokeshbalaji906@gmail.com>
2026-01-02 13:00:06 +0530

    Add UserDAO for registration and login using JDBC

--- Personal_Finance_Management/src/main/java/com/demo/pfms/model/User.java
+++ Personal_Finance_Management/src/main/java/com/demo/pfms/model/User.java
@@ -9,15 +9,46 @@ public class User {				// DTO class
 	
 	public User() {}			// Zero Argument Constructor
 
-	public User(int userId, String name, String email, String password) {		// Parameterized Constructor
-		this.userId = userId;
+	public User(String name, String email, String password) {		// Parameterized Constructor	
 		this.name = name;
 		this.email = email;
 		this.password = password;
 	}
 
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	@Override
-	public String toString() {			// toString
+	public String toString() {			// toString()
 		return "User [userId=" + userId + ", name=" + name + ", email=" + email + ", password=" + password + "]";
 	}
 }
